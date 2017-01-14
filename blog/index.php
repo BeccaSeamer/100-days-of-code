8ffb4412fb41308bfb55c1d57154171f6e47ed7c
Becca Seamer <becca.seamer@gmail.com>
2017-01-14 14:26:17 +0000

    Finished blog homepage
    Includes universal header and footer.

---
+++ blog/index.php
@@ -0,0 +1,157 @@
+<?php include("header.php"); ?>
+		
+		<div class="jumbotron">
+		
+			<div class="container">
+		
+				<h1 class="display-3 light-text">Title</h1>
+				
+				<p class="lead light-text">Your text here</p>
+				
+				<hr class="my-4 light-object">
+				
+				<p class="light-text">More text here</p>
+				
+			</div>
+		
+		</div>
+		
+		<div class="container">
+		
+			<div class="row">
+			
+				<div class="col-lg-9">
+				
+					<h1 class="page-header" id="header">Featured Posts</h1>
+				
+				</div>
+				
+			</div>
+			
+			<hr>
+			
+			<div class="row">
+			
+				<div class="col-lg-9">
+			
+					<div class="row">
+						
+						<div class="col-md-3">
+						
+							<h3 class="blog-link"><a href="#">Title</a></h3>
+							
+							<p>by <a href="#">Author</a></p>
+							
+							<p>Intro text goes here</p>
+							
+							<a class="btn btn-info" href="#">Read More <i class="fa fa-angle-right"></i></a>
+						
+						</div>
+						
+						<div class="col-md-6">
+						
+							<a href="#">
+							
+								<img class="img-responsive" src="http://placehold.it/525x300" alt="">
+								
+							</a>
+						
+						</div>
+					
+					</div>
+				
+					<hr>
+				
+					<div class="row">
+						
+						<div class="col-md-3">
+						
+							<h3 class="blog-link"><a href="#">Title</a></h3>
+							
+							<p>by <a href="#">Author</a></p>
+							
+							<p>Intro text goes here</p>
+							
+							<a class="btn btn-info" href="#">Read More <i class="fa fa-angle-right"></i></a>
+						
+						</div>
+						
+						<div class="col-md-6">
+						
+							<a href="#">
+							
+								<img class="img-responsive" src="http://placehold.it/525x300" alt="">
+								
+							</a>
+						
+						</div>
+					
+					</div>
+			
+				</div>
+				
+				<div class="col-lg-3">
+				
+					<h3>Top Posts</h3>
+					
+					<hr>
+						
+					<div class="container">
+						
+						<h5 class="blog-link"><a href="#">Title</a></h5>
+						
+						<p>Intro text goes here</p>
+						
+					</div>
+					
+					<div class="container">
+						
+						<h5 class="blog-link"><a href="#">Title</a></h5>
+						
+						<p>Intro text goes here</p>
+						
+					</div>
+					
+					<div class="container">
+						
+						<h5 class="blog-link"><a href="#">Title</a></h5>
+						
+						<p>Intro text goes here</p>
+						
+					</div>
+					
+					<hr>
+					
+					<h3>Recent Posts</h3>
+					
+					<hr>
+					
+					<div class="container">
+						
+						<h5 class="blog-link"><a href="#">Title</a></h5>
+						
+						<p>Intro text goes here</p>
+						
+					</div>
+					
+					<div class="container">
+						
+						<h5 class="blog-link"><a href="#">Title</a></h5>
+						
+						<p>Intro text goes here</p>
+						
+					</div>
+					
+					<div class="container">
+						
+						<h5 class="blog-link"><a href="#">Title</a></h5>
+						
+						<p>Intro text goes here</p>
+						
+					</div>
+				
+				</div>
+			
+			</div>
+		
+<?php include("footer.php"); ?>
