e4a86224418930ac02b8ee977a3cadf2c0d4acd9
Becca Seamer <becca.seamer@gmail.com>
2017-01-14 14:21:41 +0000

    Create header.php
    Universal header file for blog project

---
+++ blog/header.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+	<head>
+    
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		
+		<script src="https://use.fontawesome.com/c880b66bf8.js"></script>
+		
+		<style type="text/css">
+		
+			#header {
+			
+				margin-top: 10px;
+			
+			}
+			
+			.jumbotron {
+			
+				background: #000 url("header-background-image.jpg") center center;
+				background-size: cover;
+				background-repeat: no-repeat;
+				overflow: hidden;
+			
+			}
+			
+			.light-text {
+			
+				color: white;
+			
+			}
+			
+			.light-object {
+			
+				background-color: white;
+			
+			}
+			
+			a:hover {
+			
+				text-decoration: none !important;
+			
+			}
+		
+		</style>
+		
+	</head>
+  
+	<body>
+		
+		<nav class="navbar navbar-toggleable-md sticky-top navbar-light" style="background-color: #E3F2FD;">
+		
+			<div class="container">
+			
+				<a class="navbar-brand" href="index.html">Blog</a>
+			
+				<ul class="navbar-nav mr-auto">
+			
+					<li class="nav-item">
+				
+						<a class="nav-link" href="index.html">Home</a>
+				
+					</li>
+				
+					<li class="nav-item dropdown">
+				
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown">Posts</a>
+						
+						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						
+							<a class="dropdown-item" href="#">January</a>
+							
+						</div>
+				
+					</li>
+					
+					<li class="nav-item">
+					
+						<a class="nav-link" href="#">About</a>
+					
+					</li>
+					
+					<li class="nav-item">
+					
+						<a class="nav-link" href="#">Contact</a>
+					
+					</li>
+				
+				</ul>
+		
+			</div>
+		
+		</nav>
