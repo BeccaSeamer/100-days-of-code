c02b31cfd5fa8c9ba0835b5c90e9c0f9558a65b6
Becca Seamer <becca.seamer@gmail.com>
2017-01-14 15:17:26 +0000

    Finished About Page
    Includes universal header and footer. May require some layout tinkering when the content has been written.

---
+++ blog/about.php
@@ -0,0 +1,35 @@
+<?php include("header.php"); ?>
+
+		<div class="container">
+		
+			<div class="row">
+			
+				<div class="col-lg-12">
+				
+					<h1 class="page-header" id="header">About</h1>
+				
+				</div>
+			
+			</div>
+			
+			<hr>
+			
+			<div class="row">
+			
+				<div class="col-md-3">
+			
+					<img class="img-responsive" src="http://placehold.it/225x350" alt="">
+					
+				</div>
+				
+				<div class="col-md-9">
+				
+					<p>About text goes here</p>
+				
+				</div>
+			
+			</div>
+		
+		</div>
+
+<?php include("footer.php"); ?>
